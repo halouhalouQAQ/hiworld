a4ac1da0569e6648d36410a6a0f98d7eb3771c91
dong <738641868@qq.com>
2022-07-12 11:02:18 +0800

    .

--- src/main/java/com/dong/Demo01.java
+++
@@ -1,9 +0,0 @@
-package com.dong;
-
-public class Demo01{
-    public static void main(String[] args) {
-        int i = 22;
-	int b = 23;
-        System.out.println(i);
-    }
-}
--- src/main/java/com/han/HiWorld.java
+++
@@ -1,7 +0,0 @@
-package com.han;
-
-public class HiWorld {
-    public static void main(String[] args) {
-        System.out.println("Hi World!!!!");
-    }
-}
